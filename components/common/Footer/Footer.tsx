439c4b166c638a3bf6b9d417360fb12808368d3c
dan couzens <dancouzens@stus-imac.mastersgolf.co.uk>
2021-08-12 13:14:42 +0100

    footer responsive

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -27,60 +27,91 @@ const Footer = () => {
             <p>United Kingdom</p>
           </div>
         </div>
-        <div className={s.footerElements}>
-          <h2>Products</h2>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-        </div>
-        <div className={s.footerElements}>
-          <h2>Information</h2>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-        </div>
-        <div className={s.footerElements}>
-          <h2>Quick Links</h2>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-          <Link href="/">
-            <a>Test Links</a>
-          </Link>
-        </div>
-        <div className={s.footerElements}>
-          <h2>Connect</h2>
-          <div className={s.socialWrap}>
+        <div className={s.footerElementsWrap}>
+          <div className={s.footerElements}>
+            <h2>Products</h2>
             <Link href="/">
-              <a>Test Links</a>
+              <a>Play</a>
             </Link>
             <Link href="/">
-              <a>Test Links</a>
+              <a>Champion</a>
+            </Link>
+          </div>
+          <div className={s.footerElements}>
+            <h2>Information</h2>
+            <Link href="/">
+              <a>Shipping &amp; Returns</a>
             </Link>
             <Link href="/">
-              <a>Test Links</a>
+              <a>Contact Us</a>
             </Link>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+            <Link href="/">
+              <a>Sitemap</a>
+            </Link>
+          </div>
+          <div className={s.footerElements}>
+            <h2>Quick Links</h2>
+            <Link href="/games">
+              <a>Games</a>
+            </Link>
+            <Link href="/brand">
+              <a>Brand</a>
+            </Link>
+            <Link href="/">
+              <a>Stories</a>
+            </Link>
+            <Link href="/">
+              <a>Academy</a>
+            </Link>
+            <Link href="/">
+              <a>Kid Zone</a>
+            </Link>
+          </div>
+          <div className={s.footerElements}>
+            <h2>Connect</h2>
+            <div className={s.socialWrap}>
+              <a
+                href="https://twitter.com/GolfwayOfficial"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  src="/twitter.svg"
+                  height="20"
+                  width="20"
+                  alt="twitter icon"
+                />
+              </a>
+
+              <a
+                href="https://www.facebook.com/golfway.official/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  src="/facebook.svg"
+                  height="20"
+                  width="20"
+                  alt="facebook icon"
+                />
+              </a>
+
+              <a
+                href="https://www.instagram.com/golfway_official/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  src="/instagram.svg"
+                  height="20"
+                  width="20"
+                  alt="instagram icon"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -98,7 +129,7 @@ const Footer = () => {
         </div>
 
         <div className={s.copyrite}>
-          <span>&copy; {year} Golfway, Inc. All rights reserved.</span>
+          <span>&copy; Copywrite Golfway {year}. All rights reserved.</span>
         </div>
       </div>
     </footer>
