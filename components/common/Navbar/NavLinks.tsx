bd4875eeea0d7b50bafdddb33953eaa1a2ecdf3e
dan couzens <dancouzens@stus-imac.mastersgolf.co.uk>
2021-08-12 09:49:59 +0100

    nav menu

--- components/common/Navbar/NavLinks.tsx
+++ components/common/Navbar/NavLinks.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import s from './Navbar.module.scss'
 import { useRouter } from 'next/router'
 
 export default function Navbar() {
+  const [navState, setNavState] = useState(false)
+  const navButton = navState ? s.opened : ' '
+  const navMenu = navState ? s.navShow : s.navHide
+
   const router = useRouter()
   return (
     <>
-      <nav className={s.navMenu}>
+      <button
+        className={`${s.menu} ${navButton}`}
+        onClick={() => setNavState(!navState)}
+        aria-label="Main Menu"
+      >
+        <svg width="40" height="40" viewBox="0 0 100 100">
+          <path
+            className={`${s.line} ${s.line1}`}
+            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+          />
+          <path className={`${s.line} ${s.line2}`} d="M 20,50 H 80" />
+          <path
+            className={`${s.line} ${s.line3}`}
+            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+          />
+        </svg>
+      </button>
+      <nav className={`${s.navMenu} ${navMenu}`}>
         <Link href="/">
           <a className={`${router.pathname == '/' ? s.active : s.link}`}>
             Home
