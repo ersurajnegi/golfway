fbbcceda05fc700dcc115d37260481162c76acda
dan couzens <dancouzens@stus-imac.mastersgolf.co.uk>
2021-08-13 11:18:07 +0100

    nav fix

--- components/common/Navbar/NavLinks.tsx
+++ components/common/Navbar/NavLinks.tsx
@@ -8,6 +8,17 @@ export default function Navbar() {
   const navButton = navState ? s.opened : ' '
   const navMenu = navState ? s.navShow : s.navHide
 
+  const navigation = [
+    { link: '/', text: 'Home', key: 10 },
+    { link: '/games', text: 'Games', key: 20 },
+    { link: '/brand', text: 'Brand', key: 30 },
+    { link: '/search', text: 'Products', key: 40 },
+    { link: '/stories', text: 'Stories', key: 50 },
+    { link: '/recycle', text: 'Recycle', key: 60 },
+    { link: '/bogs', text: 'Blogs', key: 70 },
+    { link: '/kidzone', text: 'KidZone', key: 80 },
+  ]
+
   const router = useRouter()
   return (
     <>
@@ -29,105 +40,16 @@ export default function Navbar() {
         </svg>
       </button>
       <nav className={`${s.navMenu} ${navMenu}`}>
-        <button
-          className={`${s.menu} ${s.menuButton} ${navButton}`}
-          onClick={() => setNavState(!navState)}
-          aria-label="Main Menu"
-        >
-          <svg width="30" height="30" viewBox="0 0 100 100">
-            <path
-              className={`${s.line} ${s.line1}`}
-              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
-            />
-            <path className={`${s.line} ${s.line2}`} d="M 20,50 H 80" />
-            <path
-              className={`${s.line} ${s.line3}`}
-              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
-            />
-          </svg>
-        </button>
-        <Link href="/">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/' ? s.active : s.link}`}
-          >
-            Home
-          </a>
-        </Link>
-        <Link href="/games">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/games' ? s.active : s.link}`}
-          >
-            Games
-          </a>
-        </Link>
-        <Link href="/brand">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/brand' ? s.active : s.link}`}
-          >
-            Brand
-          </a>
-        </Link>
-        <Link href="/search">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/search' ? s.active : s.link}`}
-          >
-            Products
-          </a>
-        </Link>
-        <Link href="/stories">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/stories' ? s.active : s.link}`}
-          >
-            Stories
-          </a>
-        </Link>
-        {/* <Link href="/">
-          <a className={`${router.pathname == '/' ? s.active : s.link}`}>
-            Academy
-          </a>
-        </Link> */}
-        {/* <Link href="/">
-          <a className={`${router.pathname == '/' ? s.active : s.link}`}>
-            Tournament
-          </a>
-        </Link> */}
-        <Link href="/recycle">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/recycle' ? s.active : s.link}`}
-          >
-            Recycle
-          </a>
-        </Link>
-        <Link href="/blogs">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/blogs' ? s.active : s.link}`}
-          >
-            Blogs
-          </a>
-        </Link>
-        <Link href="/">
-          <a
-            onClick={() => setNavState(!navState)}
-            className={`${router.pathname == '/kidzone' ? s.active : s.link}`}
-          >
-            KidZone
-          </a>
-        </Link>
-        <li onClick={() => setNavState(!navState)} className={s.donate}>
-          <a href="/">Donate</a>
-        </li>
-        {/* {links?.map((l) => (
-          <Link href={l.href} key={l.href}>
-            <a className={s.link}>{l.label}</a>
+        {navigation.map((nav) => (
+          <Link key={nav.key} href={nav.link}>
+            <a className={`${router.pathname == nav.link ? s.active : s.link}`}>
+              {nav.text}
+            </a>
           </Link>
-        ))} */}
+        ))}
+        <Link href="/">
+          <a className={s.donate}>Donate</a>
+        </Link>
       </nav>
     </>
   )
