9ea2d5f827c884dadc247140a9e2721c480d91f8
dan couzens <dancouzens@stus-imac.mastersgolf.co.uk>
2021-08-25 15:43:37 +0100

    final checkout for now

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -20,7 +20,6 @@ export async function getStaticProps({
     variables: { first: 6 },
     config,
     preview,
-    // Saleor provider only
     ...({ featured: true } as any),
   })
   const pagesPromise = commerce.getAllPages({ config, preview })
@@ -48,27 +47,11 @@ export async function getStaticProps({
   }
 }
 
-// embedCheckout({
-//   containerId: 'golfway-checkout',
-//   url: 'https://masters-golf-company-store-2.mybigcommerce.com/cart.php?embedded=1&action=loadInCheckout&id=1cb89bf4-1f4b-47ad-b4c8-d4776cfcca61&token=fd94dcf5cee9067a069ed45af168397aa16e58ccb74b26e16966058d0995a22e',
-// })
-
 export default function Checkout(
   { home, products }: { products: any; home: any },
   {}: InferGetStaticPropsType<typeof getStaticProps>
 ) {
   const { heroImage, heroText, firstSection } = home[0].fields
-
-  // const testCheckout = async () => {
-  //   const module = await (window as any).checkoutKitLoader.load('checkout-sdk')
-  //   const url =
-  //     (window as any).checkoutUrl ||
-  //     'https://masters-golf-company-store-2.mybigcommerce.com/cart.php?embedded=1&action=loadInCheckout&id=8a8f2fdc-d673-4694-96a0-1abf6b8d1cb6&token=533fed504d932f42d9303dc4cdab499dd84a0286ad6f76f40ef3dee9c6a16a07'
-  //   const service = module.embedCheckout({
-  //     containerId: 'golfway-checkout',
-  //     url,
-  //   })
-  // }
 
   useEffect(() => {
     const testCheckout = async () => {
@@ -87,7 +70,9 @@ export default function Checkout(
   }, [])
   return (
     <div className={s.pageWrap}>
-      <div className={s.checkout}>{/* <h1>Temporary Checkout</h1> */}</div>
+      <div className={s.checkout}>
+        <h1>Temporary Checkout</h1>
+      </div>
       <div id="golfway-checkout"></div>
     </div>
   )
